f346d73d13a1e52dbcd56902fc7e77b28659a8eb
ChinCluBi <chinnaporn.s@ku.th>
2017-02-21 20:46:59 +0700

    :sparkles: added User class

--- example.php
+++ example.php
@@ -4,6 +4,9 @@ require(__DIR__ . '/vendor/autoload.php');
 
 use Interact\Client;
 use Interact\CurlConfig;
+use Interact\User;
 
-print_r(Client::world());
-print_r(CurlConfig::loadConfig());
+// print_r(Client::world());
+// print_r(CurlConfig::loadConfig("BASDSAD", "ASDASDASD"));
+$user = new User("asdsad", "dsadsad", [["name" => "123123", "version" => "a"], ["name" => "asdasd", "version" => "b"]]);
+var_dump($user);
---
+++ src/Interact/User.php
@@ -0,0 +1,36 @@
+<?php
+namespace Interact;
+
+use Interact\Feature;
+
+class User
+{
+    public function __construct($deviceCode, $hashedUserId, $featureList) 
+    {
+        $this->deviceCode = $deviceCode;
+        $this->hashedUserId = $hashedUserId;
+        $this->featureList = array_reduce($featureList, array($this, "reduceFeature"), []);
+    }
+
+    public function getDeviceCode()
+    {
+        return $this->deviceCode;
+    }
+
+    public function getHashedUserId()
+    {
+        return $this->hashedUserId;
+    }
+
+    public function getFeature($featureName)
+    {
+        return $this->featureList[$featureName];
+    }
+
+    
+    private function reduceFeature($carry, $feature) 
+    {
+        $carry[$feature['name']] = new Feature($feature['name'], $feature['version']);
+        return $carry;
+    }
+}
