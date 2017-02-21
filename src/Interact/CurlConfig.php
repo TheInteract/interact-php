e521e0c4006be727ef4eeafc3d5fd0d708814545
ChinCluBi <chinnaporn.s@ku.th>
2017-02-21 18:42:17 +0700

    completed curl from mockServer

--- src/Interact/CurlConfig.php
+++ src/Interact/CurlConfig.php
@@ -1,26 +1,39 @@
 <?php
 namespace Interact;
 
+use Exception;
 use Interact\Config;
 
 class CurlConfig 
 {
-    public static function loadConfig()
+    public static function doCurl()
     {
         $ch = curl_init();
         $CONFIGS = Config::getAll();
         $CONFIGS_SERVER = $CONFIGS['server'];
-        $url = $CONFIGS_SERVER['host'] . ":" . $CONFIGS_SERVER['port'];
-
+        $url = "http://" . $CONFIGS_SERVER['host'] . ":" . $CONFIGS_SERVER['port'] . "/api/event/oninit";
+        
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $response = curl_getinfo($ch);
+        $response = curl_exec($ch);
 
+
+        if ($response === FALSE) {
+            throw new Exception(curl_error($ch), curl_errno($ch));
+        }
         curl_close($ch);
 
-        var_dump($response);
         return $response;
+    }
+
+    public static function loadConfig()
+    {
+        try {
+            return CurlConfig::doCurl();
+        } catch(Exception $e) {
+            print_r($e->getCode() . " " . $e->getMessage());
+        }
     }
 }
