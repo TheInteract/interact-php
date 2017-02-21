b3c77ee69d8d65f092bc2104bc63df6f646eb73c
ChinCluBi <chinnaporn.s@ku.th>
2017-02-21 19:00:42 +0700

    receive customerCode and userIdentity on doCurl method

--- src/Interact/CurlConfig.php
+++ src/Interact/CurlConfig.php
@@ -6,16 +6,19 @@ use Interact\Config;
 
 class CurlConfig 
 {
-    public static function doCurl()
+    public static function doCurl($customerCode, $userIdentity)
     {
         $ch = curl_init();
         $CONFIGS = Config::getAll();
         $CONFIGS_SERVER = $CONFIGS['server'];
         $url = "http://" . $CONFIGS_SERVER['host'] . ":" . $CONFIGS_SERVER['port'] . "/api/event/oninit";
+        $body = ['customerCode' => $customerCode, 'userIdentity' => $userIdentity];
         
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 
         $response = curl_exec($ch);
 
@@ -28,10 +31,10 @@ class CurlConfig
         return $response;
     }
 
-    public static function loadConfig()
+    public static function loadConfig($customerCode, $userIdentity)
     {
         try {
-            return CurlConfig::doCurl();
+            return CurlConfig::doCurl($customerCode, $userIdentity);
         } catch(Exception $e) {
             print_r($e->getCode() . " " . $e->getMessage());
         }
