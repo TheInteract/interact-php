79962c974913ff0a6bc8ac53871e1e71e86c8e35
ChinCluBi <chinnaporn.s@ku.th>
2017-04-23 21:54:44 +0700

    update domain name

--- src/Interact/Config.php
+++ src/Interact/Config.php
@@ -2,7 +2,7 @@
 namespace Interact;
 
 class Config {
-    const SERVER_HOST = "35.190.0.0";
+    const SERVER_HOST = "api.interact.today";
     const SERVER_PORT = "80";
     const COOKIE_DEVICE_CODE = "interact-device-code";
     const COOKIE_USER_CODE = "interact-user-code";
